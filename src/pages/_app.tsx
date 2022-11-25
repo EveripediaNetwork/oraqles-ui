01cfcdff380b5e1dd9daa110df0a753c1a09e927
Adebesin Cell <adebesintolulope80@gmail.com>
2022-11-25 14:52:04 +0100

    Added chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import type { AppProps } from 'next/app'
 import React, { StrictMode } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import chakraTheme from '../theme'
+
 
 const App = (props: AppProps) => {
   const { Component, pageProps } = props
 
   return (
     <StrictMode>
-      <Component {...pageProps} />
+      <ChakraProvider resetCSS theme={chakraTheme}>
+        <Component {...pageProps} /> 
+      </ChakraProvider>
     </StrictMode>
   )
 }
