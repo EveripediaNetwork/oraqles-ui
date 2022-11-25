2a57a7ee6d1fc35eb685fea26bdcbb2135355988
Drebakare <bakaredmlr@gmail.com>
2022-11-25 12:04:17 +0100

    update package.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack5: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
-    return config
-  },
-  styledComponents: true,
 }
 
 module.exports = nextConfig
+
+
