a17ca0ae4fab5a9676aa7d84c89cf0967b332ed6
Drebakare <bakaredmlr@gmail.com>
2022-11-25 11:55:12 +0100

    adds theme folder

---
+++ src/theme/styles.ts
@@ -0,0 +1,10 @@
+export const styles = {
+  global: {
+    body: {
+      fontFamily: 'body',
+      bg: 'bodyBg',
+      transition: 'background-color 0.2s',
+      lineHeight: 'base',
+    },
+  },
+}
