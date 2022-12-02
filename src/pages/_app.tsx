e72bfe18eecfe13b29ae0976783c3c8c06c3ae7b
Drebakare <bakaredmlr@gmail.com>
2022-12-02 12:46:37 +0100

    fixes lint error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,10 @@ import React, { StrictMode } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '@/components/Layout/Layout/Layout'
 import { Montserrat } from '@next/font/google'
-import chakraTheme from '../theme'
 import { createClient, WagmiConfig } from 'wagmi'
 import { provider } from '@/config/wagmi'
+import chakraTheme from '../theme'
 import '../utils/i18n'
-
 
 type OraqleAppProp = Omit<AppProps, 'Component'> & {
   Component: AppProps['Component'] & { noFooter?: boolean }
