3e23a72ca96a765e0e95744fe414216273496c7b
Adebesin Cell <adebesintolulope80@gmail.com>
2022-12-06 08:08:31 +0100

    Fixes mobile menu socials

--- src/components/Layout/Navbar/MobileNav.tsx
+++ src/components/Layout/Navbar/MobileNav.tsx
@@ -1,23 +1,33 @@
 import { NAV_ITEMS } from '@/data/NavItemData'
-import { Box, Divider, VStack } from '@chakra-ui/react'
+import { Box, Center, Divider, Flex } from '@chakra-ui/react'
 import React from 'react'
 import MobileNavLink from '@/components/Layout/Navbar/MobileNavLink'
+import {
+  RiFacebookFill,
+  RiInstagramFill,
+  RiLinkedinFill,
+  RiTelegramFill,
+  RiTwitterFill,
+} from 'react-icons/ri'
 
 type MobileNavType = {
   setHamburger: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const MobileNav = ({ setHamburger }: MobileNavType) => {
+  const iconSize = 20
+
   return (
-    <VStack
-      display={{ md: 'none' }}
+    <Flex
+      direction="column"
+      display={{ base: 'flex', md: 'none' }}
       h={{ base: 'calc(100vh - 70px)' }}
       justifyContent="space-between"
       backgroundColor="subMenuBg"
     >
-      <Box w="full" pt="6">
+      <Box w="full">
         <Divider />
-        <Box px={{ base: '5' }}>
+        <Box px={{ base: '5' }} pt="6">
           {NAV_ITEMS.map(navlink => (
             <MobileNavLink
               setHamburger={setHamburger}
@@ -29,7 +39,22 @@ const MobileNav = ({ setHamburger }: MobileNavType) => {
           ))}
         </Box>
       </Box>
-    </VStack>
+      <Center
+        bg="subMenuBg"
+        h="80px"
+        borderTopWidth="thin"
+        borderTopColor="borderColor"
+        color="homeDescriptionColor"
+      >
+        <Flex gap={8}>
+          <RiInstagramFill size={iconSize} />
+          <RiLinkedinFill size={iconSize} />
+          <RiFacebookFill size={iconSize} />
+          <RiTelegramFill size={iconSize} />
+          <RiTwitterFill size={iconSize} />
+        </Flex>
+      </Center>
+    </Flex>
   )
 }
 
