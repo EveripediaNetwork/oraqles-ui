24c4b10e08df4e774a5cd77413a0088e3911ec19
Adebesin Cell <adebesintolulope80@gmail.com>
2022-12-06 13:07:12 +0100

    Added default SEO

--- src/components/SEO/default.tsx
+++ src/components/SEO/default.tsx
@@ -10,7 +10,6 @@ const SEOHeader = ({ router }: SEOHeaderProps) => {
   return (
     <DefaultSeo
       title="IQ Oraqles"
-      titleTemplate="%s | IQ Oraqles"
       description="IQ OraQles is a service managed by BrainDAO that allows verified first parties to bring real-world information on-chain."
       canonical={`https://oraqles.com${router.asPath || ''}`}
       openGraph={{
