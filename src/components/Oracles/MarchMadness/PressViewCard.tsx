1a910a10af1bcb5f9a6d4b169e0d6d34613136ce
OleanjiKingCode <holamilean56@gmail.com>
2022-12-12 10:39:20 +0100

    press cards in march-madness

--- src/components/Oracles/MarchMadness/PressViewCard.tsx
+++ src/components/Oracles/MarchMadness/PressViewCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Box, Flex, Image } from '@chakra-ui/react'
+import { Box, Flex, Text, chakra, Image, VStack } from '@chakra-ui/react'
 import { PressJsonprops } from '@/data/MarchMadnessPressData'
+import Link from 'next/link'
 
 const MarchMadnessPressViewCard = ({
   image,
@@ -8,7 +9,7 @@ const MarchMadnessPressViewCard = ({
   text,
   link,
 }: PressJsonprops) => {
-  console.log(title, text, link)
+  console.log(title, text, link, image)
 
   return (
     <Flex
@@ -17,6 +18,8 @@ const MarchMadnessPressViewCard = ({
       minH="350px"
       justifyContent="space-between"
       borderColor="oraclesPressViewCardBorder"
+      cursor="pointer"
+      rounded="lg"
     >
       <Box>
         <Image
@@ -26,7 +29,33 @@ const MarchMadnessPressViewCard = ({
           h="220px"
           objectFit="cover"
           bgColor="#FAFAFA"
+          borderRadius="none"
+          roundedTop="lg"
         />
+        <VStack pt="4" w="full">
+          <chakra.div w="full" px="3">
+            <Link href={link} passHref target="_blank">
+              <Text fontWeight="bold">VIEW</Text>
+            </Link>
+          </chakra.div>
+
+          <chakra.div px="3" fontWeight="light">
+            {text}
+          </chakra.div>
+          <chakra.div
+            borderRadius="none"
+            roundedBottom="lg"
+            py="2"
+            px="2"
+            fontWeight="light"
+            bg="gray.50"
+            w="full"
+            position="relative"
+            bottom="0"
+          >
+            {title}
+          </chakra.div>
+        </VStack>
       </Box>
     </Flex>
   )
