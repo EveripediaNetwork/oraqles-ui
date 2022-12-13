8e4dae4467bfc902bb0ece3f5f2f72a6f5d86ffb
Adebesin Cell <adebesintolulope80@gmail.com>
2022-12-13 21:35:26 +0100

    Added theme swticher mobile

--- src/components/Layout/Navbar/MobileNav.tsx
+++ src/components/Layout/Navbar/MobileNav.tsx
@@ -1,5 +1,14 @@
 import { NAV_ITEMS } from '@/data/NavItemData'
-import { Box, Center, Divider, Flex } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  Flex,
+  IconButton,
+  Text,
+  useColorMode,
+} from '@chakra-ui/react'
 import React from 'react'
 import MobileNavLink from '@/components/Layout/Navbar/MobileNavLink'
 import {
@@ -8,6 +17,8 @@ import {
   RiLinkedinFill,
   RiTelegramFill,
   RiTwitterFill,
+  RiSunFill,
+  RiMoonFill,
 } from 'react-icons/ri'
 
 type MobileNavType = {
@@ -15,6 +26,8 @@ type MobileNavType = {
 }
 
 const MobileNav = ({ setHamburger }: MobileNavType) => {
+  const { colorMode, toggleColorMode } = useColorMode()
+  const ColorModeIcon = colorMode === 'light' ? RiMoonFill : RiSunFill
   const iconSize = 20
 
   return (
@@ -37,6 +50,28 @@ const MobileNav = ({ setHamburger }: MobileNavType) => {
               label={navlink.label}
             />
           ))}
+          <Button
+            w="full"
+            display="flex"
+            variant="unstyled"
+            justifyContent="space-between"
+            onClick={toggleColorMode}
+            px="4 !important"
+            py="6 !important"
+          >
+            <Text fontWeight={400}>
+              {colorMode === 'light' ? 'Dark mode' : 'Light Mode'}
+            </Text>
+            <IconButton
+              aria-label="Color mode"
+              icon={<ColorModeIcon />}
+              variant="unstyled"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              size="md"
+            />
+          </Button>
         </Box>
       </Box>
       <Center
